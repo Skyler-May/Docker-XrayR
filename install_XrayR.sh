608a7c87838f4e112c9d6dabf0fdd8f2b4e1c499
Skyler-May <meisongyong@gmail.com>
2025-04-28 22:44:09 +0800

    Initial commit

--- install_XrayR.sh
+++ install_XrayR.sh
@@ -140,7 +140,7 @@ curl -fsSL https://get.docker.com | bash -s docker
 echo_color "green" "完成"
 
 echo_color "green" "安装Docker-compose"
-curl -L "https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.35.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 echo_color "green" "安装完成"
 
 echo_color "green" "赋予docker-compose执行权限"
